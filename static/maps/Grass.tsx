1bf4121789e9e15bdd31700db36fefd9745d26f7
Trezy <tre@trezy.com>
2019-02-13 21:04:25 -0600

    feat(maps): Convert maps to Tiled Map Editor formats

---
+++ static/maps/Grass.tsx
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Grass" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="grass.png" width="256" height="128"/>
+ <terraintypes>
+  <terrain name="Grass" tile="26"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="2" terrain="0,0,0,0"/>
+ <tile id="3" terrain="0,0,0,0"/>
+ <tile id="4" terrain="0,0,0,0"/>
+ <tile id="5" terrain="0,0,0,0"/>
+ <tile id="6" terrain="0,0,0,0"/>
+ <tile id="7" terrain="0,0,0,0"/>
+ <tile id="8" terrain="0,0,0,0"/>
+ <tile id="9" terrain="0,0,0,0"/>
+ <tile id="10" terrain="0,0,0,0"/>
+ <tile id="11" terrain="0,0,0,0"/>
+ <tile id="12" terrain="0,0,0,0"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="14" terrain="0,0,0,0"/>
+ <tile id="15" terrain="0,0,0,0"/>
+ <tile id="16" terrain="0,0,0,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="0,0,0,0"/>
+ <tile id="19" terrain="0,0,0,0"/>
+ <tile id="20" terrain="0,0,0,0"/>
+ <tile id="21" terrain="0,0,0,0"/>
+ <tile id="22" terrain="0,0,0,0"/>
+ <tile id="23" terrain="0,0,0,0"/>
+ <tile id="24" terrain="0,0,0,0" probability="0.5">
+  <objectgroup draworder="index">
+   <object id="2" x="2.76734" y="21.8474" width="17.3323" height="5.24338"/>
+  </objectgroup>
+ </tile>
+ <tile id="25" terrain="0,0,0,0" probability="0.5">
+  <objectgroup draworder="index">
+   <object id="3" x="8.66614" y="19.1529" width="17.5508" height="6.99117"/>
+  </objectgroup>
+ </tile>
+ <tile id="26" terrain="0,0,0,0" probability="100"/>
+ <tile id="27" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="28" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="29" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="30" terrain="0,0,0,0" probability="0.3"/>
+ <tile id="31" terrain="0,0,0,0" probability="0.3"/>
+</tileset>
