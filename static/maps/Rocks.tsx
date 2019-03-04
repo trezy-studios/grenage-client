0c74406c07197be345c6fb087c5402a115659b45
Trezy <tre@trezy.com>
2019-03-04 00:35:16 -0600

    feat: New maps, yo!

---
+++ static/maps/Rocks.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Rocks" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="Rocks.png" width="96" height="64"/>
+ <tile id="0" probability="0.1"/>
+ <tile id="1" probability="0.1"/>
+ <tile id="2" probability="0.1"/>
+ <tile id="3" probability="0.05">
+  <objectgroup draworder="index">
+   <object id="1" x="8" y="19" width="17" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="4" probability="0.05">
+  <objectgroup draworder="index">
+   <object id="1" x="8" y="14" width="17" height="11"/>
+  </objectgroup>
+ </tile>
+ <tile id="5" probability="10"/>
+</tileset>
